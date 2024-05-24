4ffbb05992104736b584c387a174a38a4aa47874
M Haikal Maulidy <113519882+MHaikalMaulidy@users.noreply.github.com>
2024-05-24 16:49:04 +0700

    Add files via upload

---
+++ Javascript-dasar/variabel.js
@@ -0,0 +1,22 @@
+var nama1='timothy ';
+var nama2=nama1+'ronald';
+document.writeln(nama2)
+nama2='icikwir '+nama1
+document.writeln('</br>')
+document.writeln(nama2)
+document.writeln('</br>')
+/*jangan pake syntax var lagi
+pake let & const 
+var sama kaya let*/
+let nama3='timothy ';
+let nama4=nama3+'ronald';
+document.writeln(nama4)
+nama4='icikwir '+nama3
+document.writeln('</br>')
+document.writeln(nama4)
+
+/*const variabel yang gabisa diubah lagi datanya 
+biasanya buat nama web,versi aplikasi, dll*/
+const fixBanget='ga bisa diubah kalo diubah error'
+document.writeln('</br>')
+document.writeln(fixBanget)
