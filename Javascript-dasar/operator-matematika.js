858864e6f3d0f1be2b14c6d9834652b25c60a219
M Haikal Maulidy <113519882+MHaikalMaulidy@users.noreply.github.com>
2024-05-25 11:15:25 +0700

    Add files via upload

---
+++ Javascript-dasar/operator-matematika.js
@@ -0,0 +1,8 @@
+/*sama kaya python*/
+let hasil=1*2;
+document.writeln("</p> 1 x 2 = ",hasil,"</p>")
+let hasilPangkat= hasil ** 10;
+document.writeln("</p> 2^10 = ",hasilPangkat,"</p>")
+hasil*= 10;
+document.writeln("</p> 2 x 10 = ",hasil,"</p>")
+hasil++;
