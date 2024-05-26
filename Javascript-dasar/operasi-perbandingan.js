2f399d4d5912beb60919688ea37badacc7f86a23
M Haikal Maulidy <113519882+MHaikalMaulidy@users.noreply.github.com>
2024-05-26 13:12:31 +0700

    Add files via upload

---
+++ Javascript-dasar/operasi-perbandingan.js
@@ -0,0 +1,11 @@
+/*mirip kaya python */
+let lima = 5 == "5";
+document.writeln("<p>"+lima+"</p>")
+/* === itu nilai sama tipe datanya wajib sama kalau mau true */
+lima = 5 === "5"
+document.writeln("<p>"+lima+"</p>")
+lima = 5 != "5"
+document.writeln("<p>"+lima+"</p>")
+lima = 5 !== "5"
+document.writeln("<p>"+lima+"</p>")
+/*sisanya sama kaya python */
