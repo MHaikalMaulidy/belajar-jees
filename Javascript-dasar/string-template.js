394b3a331a7399629b0e5777a80e84958546b955
M Haikal Maulidy <113519882+MHaikalMaulidy@users.noreply.github.com>
2024-05-28 15:53:04 +0700

    Add files via upload

---
+++ Javascript-dasar/string-template.js
@@ -0,0 +1,13 @@
+/*jadi gaperlu pake + */
+const name="eko ronald"
+const lastName="timothy";
+let template=`<p>Name : ${name} ${lastName} </p>`;
+document.writeln(template)
+
+const nilai=90
+template =`nama: ${name} lulus: ${nilai>100}`
+document.writeln(template)
+const gacor=`<p>bisa gini coyy
+gacooor
+haha </p>`
+document.writeln(gacor)
