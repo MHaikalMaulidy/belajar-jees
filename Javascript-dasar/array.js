b22bad18ef67d1ccd3f2255c4e73d2ff726dfc48
M Haikal Maulidy <113519882+MHaikalMaulidy@users.noreply.github.com>
2024-05-30 14:31:07 +0700

    Add files via upload

---
+++ Javascript-dasar/array.js
@@ -0,0 +1,8 @@
+/*sama persis ky python */
+let nama=[1,1.9,true,'eko','timothy','ronald','manta gacor'];
+let arrayKosong=[];
+arrayKosong.push('gacooor','manta','wawan putih')
+arrayKosong[0]='timothy'
+console.table(arrayKosong)
+delete arrayKosong[0]
+console.table(arrayKosong)
