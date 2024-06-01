acb4b288a9a1aa6d63b8031a50ce791f50cdf94d
M Haikal Maulidy <113519882+MHaikalMaulidy@users.noreply.github.com>
2024-06-01 16:28:30 +0700

    Add files via upload

---
+++ Javascript-dasar/popup.js
@@ -0,0 +1,9 @@
+/*confirm nge return tipe data bool */
+const masuk=confirm("apakah kamu join AC? ")
+if(masuk){
+    const nama=prompt("siapa nama anda: ");
+    alert(`gacoor ${nama}`)
+}
+else{
+    alert("dasar sdm rendah, mclaren lu warna apa boss")
+}
