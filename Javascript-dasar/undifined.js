9c27ef6ec01b32ff41d04e2b683d6f6600098592
M Haikal Maulidy <113519882+MHaikalMaulidy@users.noreply.github.com>
2024-06-01 16:37:25 +0700

    Add files via upload

---
+++ Javascript-dasar/undifined.js
@@ -0,0 +1,16 @@
+/*tipe data */
+let nama;
+if (nama===undefined){
+    alert("UNDIFINED")
+}
+else{
+    alert('DIFINED')
+}
+let array=[0,1]
+if (array[3]===undefined){
+    console.info("tidak ada array index ke 3")
+}
+let person={pekerjaan:"trading"}
+if (person.nama===undefined){
+    console.info("tidak ada nama")
+}
