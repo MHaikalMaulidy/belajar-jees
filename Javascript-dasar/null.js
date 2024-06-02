d28692757a5457b79d14c19a9ae9fd2404cacfa9
M Haikal Maulidy <113519882+MHaikalMaulidy@users.noreply.github.com>
2024-06-02 09:51:11 +0700

    Add files via upload

---
+++ Javascript-dasar/null.js
@@ -0,0 +1,8 @@
+/*null adalah tipe data beda sama undifined */
+const jawa=null;
+if(jawa===undefined){
+    alert("jawa adalah undefined");
+}
+else if(jawa===null){
+    alert("jawa adalah null")
+}
