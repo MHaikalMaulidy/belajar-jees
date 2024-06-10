358f6f8f7165094eb129399886edfcc5880fa96f
M Haikal Maulidy <113519882+MHaikalMaulidy@users.noreply.github.com>
2024-06-10 08:13:51 +0700

    Add files via upload

---
+++ Javascript-dasar/withStatement.js
@@ -0,0 +1,10 @@
+const person={
+    nama:'timothy ronald',
+    pekerjaan:'tradink',
+    mobil:'mclaren'
+}
+with (person){
+    document.writeln(nama);
+    document.writeln(pekerjaan);
+    document.writeln(mobil);
+}
