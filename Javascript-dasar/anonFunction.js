ac2df0fd67c03cb508d938e8cbfab5e013a56b4e
M Haikal Maulidy <113519882+MHaikalMaulidy@users.noreply.github.com>
2024-06-15 13:24:36 +0700

    Add files via upload

---
+++ Javascript-dasar/anonFunction.js
@@ -0,0 +1,9 @@
+const rizz=function(){
+    return '<p>sigma skibidi toilet</p>'
+} 
+document.writeln(rizz())
+
+function iniFunction(masukkanFungsi){
+        masukkanFungsi('eko')
+}
+iniFunction(function(name){document.writeln(`hi ${name}`)})
