969642e73eae305feeb520fee978d95ea2b3fc7e
M Haikal Maulidy <113519882+MHaikalMaulidy@users.noreply.github.com>
2024-06-23 09:29:12 +0700

    Add files via upload

---
+++ javascript-oop/constructorClass.js
@@ -0,0 +1,7 @@
+class person{
+    constructor(name){
+        console.info(`membuat person ${name}`)
+    }
+}
+const eko=new person('eko');
+console.info(eko)
---
+++ javascript-oop/property.js
@@ -0,0 +1,7 @@
+class person{
+    constructor(name){
+        this.name=name
+    }
+}
+const eko=new person('ekoh');
+document.writeln(eko.name)
