aedf2fb28125b1d05c610aa7edf2d876794d7057
M Haikal Maulidy <113519882+MHaikalMaulidy@users.noreply.github.com>
2024-06-25 14:09:00 +0700

    Add files via upload

---
+++ javascript-standard-library/string.js
@@ -0,0 +1,7 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+const nama='Timoty Ronald'
+document.writeln(nama.toUpperCase())
+document.writeln(nama.split(' '))
+
+const input='  timur jawa  '
+document.writeln(input.trim())
