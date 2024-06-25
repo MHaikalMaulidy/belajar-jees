b3a0ee483331047cf462bc9c0f008aa81ce5d4a4
M Haikal Maulidy <113519882+MHaikalMaulidy@users.noreply.github.com>
2024-06-25 12:42:05 +0700

    Add files via upload

---
+++ javascript-standard-library/number.js
@@ -0,0 +1,7 @@
+/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number */
+const input=`12345`
+const number=Number(input)
+document.writeln(number)
+const value=12345;
+document.writeln(value.toString(2))
+document.writeln(value.toLocaleString('id-ID'))
