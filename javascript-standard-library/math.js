09937d1da6d6bff9fae3348c3ea135f7f353ade1
M Haikal Maulidy <113519882+MHaikalMaulidy@users.noreply.github.com>
2024-06-29 18:19:52 +0700

    Add files via upload

---
+++ javascript-standard-library/math.js
@@ -0,0 +1,8 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+//operasi mtk di js ada nyari nilai max,rata2,random dll
+document.writeln('<p>',Math.max(1,2,3,100,20,39,101))
+document.writeln('<p>',Math.min(10,200,1))
+//buletin kebawah
+document.writeln('<p>',Math.floor(10.8))
+document.writeln('<p>',Math.round(1.33))
+document.writeln('<p>',Math.round(1.5))
