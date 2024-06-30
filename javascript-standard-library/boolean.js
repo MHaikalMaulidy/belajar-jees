197c923e669d873bfe8e099a710a61678bb76788
M Haikal Maulidy <113519882+MHaikalMaulidy@users.noreply.github.com>
2024-06-30 12:47:40 +0700

    Add files via upload

---
+++ javascript-standard-library/boolean.js
@@ -0,0 +1,6 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean
+const bool=Boolean(false);
+document.writeln(bool)
+document.writeln('<p>',bool.valueOf())
+document.writeln('<p>',bool.toString())
+
