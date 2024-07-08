14a1b840986299d809c4afafc194ad15ae1909b0
M Haikal Maulidy <113519882+MHaikalMaulidy@users.noreply.github.com>
2024-07-08 12:26:06 +0700

    Add files via upload

---
+++ javascript-modules/exportMultiple.mjs
@@ -0,0 +1,17 @@
+const company='lethal company'
+
+function sum(first,second){
+    return first+second
+}
+
+class Person{
+    constructor(name){
+        this.name = name;
+    }
+
+    sayHello(name){
+        console.info(`hallo ${name}, my name is ${this.name}`);
+    }
+}
+
+export{company, Person,sum}
